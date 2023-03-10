a12620320a9b7d23d1feef6dee606e98885ddf11
Shashwamie <50168832+Shashwamie@users.noreply.github.com>
2023-03-09 19:02:57 -0700

    Create MouseControls.java

---
+++ src/MouseControls.java
@@ -0,0 +1,44 @@
+package util;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.SwingUtilities;
+
+public class MouseControls implements MouseListener{
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if(SwingUtilities.isRightMouseButton(e)) {
+			System.out.println("right click");
+		}else if(SwingUtilities.isLeftMouseButton(e)) {
+			System.out.println("left click");
+		}
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
