a6f30f14e411c5cadb0c5b288b5ea83ff968cb1d
Shashwamie <50168832+Shashwamie@users.noreply.github.com>
2023-03-09 19:05:44 -0700

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import board.Board;
+import gui.GUI;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Board board = new Board();
+		GUI gui = new GUI(board);
+
+	}
+
+}
