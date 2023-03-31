94867ff12b0591082575ac2ae7622224cfdd3af0
Shashwamie <50168832+Shashwamie@users.noreply.github.com>
2023-03-30 20:04:02 -0600

    Add files via upload

--- src/pieces/Piece.java
+++ src/pieces/Piece.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import board.Board;
 import board.Move;
 import util.PieceColor;
+import util.PieceType;
 
 /*
  * abstract class for all pieces for layout of pieces
@@ -14,6 +15,7 @@ public abstract class Piece {
 	int piecePos;
 	PieceColor pieceColor;
 	boolean isFirstMove;
+	PieceType pieceType;
 	
 	/*
 	 * constructor for pieces needs
@@ -25,6 +27,7 @@ public abstract class Piece {
 		this.piecePos = piecePos;
 		this.pieceColor = pieceColor;
 		this.isFirstMove = isFirstMove;
+		this.pieceType = createPieceType(this.toString());
 	}
 	
 	/*
@@ -42,6 +45,13 @@ public abstract class Piece {
 	}
 	
 	/*
+	 * getter for piece type
+	 */
+	public PieceType getPieceType() {
+		return this.pieceType;
+	}
+	
+	/*
 	 * getter for if first move
 	 */
 	public boolean isFirstMove() {
@@ -52,5 +62,17 @@ public abstract class Piece {
 	public abstract Collection<Move> calculateMoves(Board board);
 	//public abstract Collection<Move> getAttackMoves(Board board);
  	public abstract Piece movePiece(Move move);
-
+ 	
+ 	public PieceType createPieceType(String pieceLetter) {
+ 		
+ 		switch(pieceLetter) {
+ 		case "B": return PieceType.BISHOP;
+ 		case "K": return PieceType.KING;
+ 		case "N": return PieceType.KNIGHT;
+ 		case "P": return PieceType.PAWN;
+ 		case "Q": return PieceType.QUEEN;
+ 		case "R": return PieceType.ROOK;
+ 		}
+ 		return null;
+ 	}
 }
